a1e3cd90f595fb4a77cb890455602f997a8e522a
raviTechindustan <ravi.techindustan@gmail.com>
2020-05-20 21:38:01 +0530

    resolved error for netlify github

---
+++ pages/Arrivals.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default  function Arrivals() {
+    return(
+
+        <div>
+            <h1 className="text-center">Coming Soon</h1>
+        </div>
+
+    )
+}
---
+++ pages/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Contact() {
+    return(
+        <div>
+            <h1 className="text-center">
+                Coming Soon
+            </h1>
+        </div>
+    )
+}
